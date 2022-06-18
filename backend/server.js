ee3bce8dbfa689626dd823bbab537be1fbb03cf1
Aman Singh <amansingh.afk@gmail.com>
2022-06-18 06:26:16 +0530

    ../Server Workflow added.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
-const app = require(",/app");
+const app = require("./app");
+
+const dotenv = require("dotenv");
+
+//config
+
+dotenv.config({ path: "backend/config/config.env" });
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Serve is working on http://localhost:${process.env.PORT}`);
