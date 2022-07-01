d24b8c0dc7b2c5646bde8c899e032811c8aacd75
Aman Singh <amansingh.afk@gmail.com>
2022-07-02 00:59:56 +0530

    nodemailer addded;

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwttoken")
+const sendEmail = require("../utils/sendEmail")
 
 //register a user
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -53,6 +54,48 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
     }
     res.status(200).json({
         success: true,
-        message: "Logout "
+        message: "Logged out "
     });
 });
+
+//forgot password
+
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next(new ErrorHandler("user not found", 404));
+    }
+
+    //resetPassword token 
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is: \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please igonore it`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: `Ecommerce Password recovery`,
+            message,
+        });
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to ${user.email} successfully`,
+        });
+
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorHandler(error.message, 500));
+
+    }
+})
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -59,6 +60,19 @@ userSchema.methods.getJWTToken = function () {
 
 userSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//generating password reset token
+
+userSchema.methods.getResetPasswordToken = function () {
+
+    const resetToken = crypto.randomBytes(20).toString("hex");
+
+    this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+
+    this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
+    return resetToken;
 };
 
 module.exports = mongoose.model("user", userSchema);
